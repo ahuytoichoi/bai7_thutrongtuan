1c4d67ab1f5938c0e1e786c29e8601123a663fb1
MAY 06 <MAY 06@LAB2-34>
2024-11-11 14:37:07 +0700

    Add project files.

---
+++ bai7_thutrongtuan.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int so;
+	cout << "nhap ngay"; cin >> so;
+		switch (so)
+		{
+		case 2:
+			cout << "thu hai\n";
+			break;
+		case 3:
+			cout << "thu ba\n";
+			break;
+		case 4:
+			cout << "thu bon\n";
+			break;
+		case 5:
+			cout << "thu nam\n";
+			break;
+		case 6:
+			cout << "thu sau\n";
+			break;
+		case 7:
+			cout << "thu bay\n";
+			break;
+		case 8:
+			cout << "chu nhat\n";
+			break;
+		default:
+			cout << "khong phai cac ngay trong tuan";
+			break;
+		}
+	return 0;
+}
